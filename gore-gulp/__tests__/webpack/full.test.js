f59146f0875d5b7596fbd6c600081fbb089dec20
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-04-08 22:51:55 +0200

    reduce webpack test file complexity

--- gore-gulp/__tests__/webpack/full.test.js
+++ gore-gulp/__tests__/webpack/full.test.js
@@ -23,6 +23,26 @@ var path = require("path"),
 describe("webpack", function () {
     var tmpDir;
 
+    function doFiles(paths, cb) {
+        return function (distDir) {
+            paths.map(function (pth) {
+                return FS.isFile(path.join(distDir, pth)).then(function (isFile) {
+                    return cb(isFile, pth);
+                });
+            });
+
+            return Q.all(paths).then(_.noop);
+        };
+    }
+
+    function expectFiles(paths) {
+        return doFiles(paths, assert.ok);
+    }
+
+    function notExpectFiles(paths) {
+        return doFiles(paths, assert.notOk);
+    }
+
     function runDirectory(baseDir) {
         var distDir;
 
@@ -56,172 +76,100 @@ describe("webpack", function () {
 
     it("generates output using .entry." + defaults.ecmaScriptFileExtensionsGlobPattern + " files", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-1"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-1.common.min.js"),
-                    path.join(distDir, "test-library-1.common.min.js.map"),
-                    path.join(distDir, "test-library-1.module.min.js"),
-                    path.join(distDir, "test-library-1.module.min.js.map"),
-                    path.join(distDir, "test-library-1.test.min.js"),
-                    path.join(distDir, "test-library-1.test.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-1.common.min.js",
+                "test-library-1.common.min.js.map",
+                "test-library-1.module.min.js",
+                "test-library-1.module.min.js.map",
+                "test-library-1.test.min.js",
+                "test-library-1.test.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("uses library location specified in package configuration", function (done) {
+        var distDir;
+
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-2"))
-            .then(function (distDir) {
-                var paths;
+            .then(function (dd) {
+                distDir = dd;
 
-                paths = [
-                    path.join(distDir, "test-library-2.common.min.js"),
-                    path.join(distDir, "test-library-2.common.min.js.map"),
-                    path.join(distDir, "test-library-2.true.min.js"),
-                    path.join(distDir, "test-library-2.true.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                paths = paths.concat([
-                    path.join(distDir, "test-library-2.not-an.min.js"),
-                    path.join(distDir, "test-library-2.not-an.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.notOk(isFile, pth);
-                    });
-                }));
-
-                return Q.all(paths);
+                return dd;
             })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-2.common.min.js",
+                "test-library-2.common.min.js.map",
+                "test-library-2.true.min.js",
+                "test-library-2.true.min.js.map"
+            ]))
+            .then(function () {
+                return distDir;
+            })
+            .then(notExpectFiles([
+                "test-library-2.not-an.min.js",
+                "test-library-2.not-an.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("uses vendor libraries configuration field", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-3"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-3.common.min.js"),
-                    path.join(distDir, "test-library-3.common.min.js.map"),
-                    path.join(distDir, "test-library-3.index.min.js"),
-                    path.join(distDir, "test-library-3.index.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-3.common.min.js",
+                "test-library-3.common.min.js.map",
+                "test-library-3.index.min.js",
+                "test-library-3.index.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("resolves nested modules paths", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-4"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-4.common.min.js"),
-                    path.join(distDir, "test-library-4.common.min.js.map"),
-                    path.join(distDir, "test-library-4.index.min.js"),
-                    path.join(distDir, "test-library-4.index.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-4.common.min.js",
+                "test-library-4.common.min.js.map",
+                "test-library-4.index.min.js",
+                "test-library-4.index.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("resolves node_modules paths", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-5"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-5.common.min.js"),
-                    path.join(distDir, "test-library-5.common.min.js.map"),
-                    path.join(distDir, "test-library-5.fake-module-holder.min.js"),
-                    path.join(distDir, "test-library-5.fake-module-holder.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-5.common.min.js",
+                "test-library-5.common.min.js.map",
+                "test-library-5.fake-module-holder.min.js",
+                "test-library-5.fake-module-holder.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("uses externals settings", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-6"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-6.common.min.js"),
-                    path.join(distDir, "test-library-6.common.min.js.map"),
-                    path.join(distDir, "test-library-6.index.min.js"),
-                    path.join(distDir, "test-library-6.index.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-6.common.min.js",
+                "test-library-6.common.min.js.map",
+                "test-library-6.index.min.js",
+                "test-library-6.index.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
 
     it("resolves multiple entry points", function (done) {
         runDirectory(path.join(__dirname, "__fixtures__", "test-library-7"))
-            .then(function (distDir) {
-                var paths;
-
-                paths = [
-                    path.join(distDir, "test-library-7.common.min.js"),
-                    path.join(distDir, "test-library-7.common.min.js.map"),
-                    path.join(distDir, "test-library-7.first-pointof.min.js"),
-                    path.join(distDir, "test-library-7.second-pointof.min.js.map")
-                ].map(function (pth) {
-                    return FS.isFile(pth).then(function (isFile) {
-                        assert.ok(isFile, pth);
-                    });
-                });
-
-                return Q.all(paths);
-            })
-            .then(_.noop)
+            .then(expectFiles([
+                "test-library-7.common.min.js",
+                "test-library-7.common.min.js.map",
+                "test-library-7.first-pointof.min.js",
+                "test-library-7.second-pointof.min.js.map"
+            ]))
             .then(done)
             .catch(done);
     });
