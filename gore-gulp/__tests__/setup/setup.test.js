650b54c4e2b9929bbac48647264eedd97774d2c7
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-04-13 14:36:18 +0200

    test setup

---
+++ gore-gulp/__tests__/setup/setup.test.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2015-present, goreutils
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+"use strict";
+
+/*global afterEach:false, beforeEach: false, describe: false, it: false */
+
+var path = require("path"),
+    assert = require("chai").assert,
+    gg = require(path.join(__dirname, "..", "..", "index"));
+
+describe("setup", function () {
+    var previousNodeEnv;
+
+    function doTestSetup(environment, taskName) {
+        var tasks = {};
+
+        process.env.NODE_ENV = environment;
+
+        gg(__dirname).setup({
+            "task": function (name, deps) {
+                tasks[name] = deps;
+            }
+        });
+
+        assert.deepEqual(tasks.webpack, [
+            taskName
+        ]);
+    }
+
+    afterEach(function () {
+        process.env.NODE_ENV = previousNodeEnv;
+    });
+
+    beforeEach(function () {
+        previousNodeEnv = process.env.NODE_ENV;
+    });
+
+    it("sets up gulp instance using development settings", function () {
+        doTestSetup("development", "webpack.quick");
+    });
+
+    it("sets up gulp instance using production settings", function () {
+        doTestSetup("production", "webpack.full");
+    });
+});
