be6038e4d2ff98bff4d5100041a8b11a1dbf7a17
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-10-08 23:20:29 +0200

    add missing modules

---
+++ gore-gulp/__tests__/plugin/webpack/hmr.test.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2015-present, goreutils
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+"use strict";
+
+/* global describe: false, it: false */
+
+var path = require("path"),
+    gg = require(path.resolve(__dirname, "..", "..", "..", "index")),
+    Gulp = require("gulp").Gulp;
+
+describe.skip("webpack/hmr", function () {
+    it("runs hot module replacement server and serves compiled bundle", function (done) {
+        var gulpInstance = new Gulp();
+
+        gg({
+            "baseDir": path.resolve(__dirname, "..", "..", "..", "..", "example", "hot-module-replacement")
+        }).setup(gulpInstance);
+
+        gulpInstance.on("err", done);
+        gulpInstance.on("stop", function () {
+            done();
+        });
+
+        gulpInstance.start("hmr");
+    });
+});
