76c0f7aa17c3c1ffc72050319926048396ef91ac
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2016-02-01 00:19:22 +0100

    add webpack babel stage-0 preset for class properties

--- gore-gulp/plugin/webpack/config/babel.js
+++ gore-gulp/plugin/webpack/config/babel.js
@@ -39,7 +39,8 @@ function babel(webpackConfig, config, pckg, babelOverride) {
                             ],
                             "presets": [
                                 require.resolve("babel-preset-es2015"),
-                                require.resolve("babel-preset-react")
+                                require.resolve("babel-preset-react"),
+                                require.resolve("babel-preset-stage-0")
                             ]
                         }, babelOverride)
                     }
